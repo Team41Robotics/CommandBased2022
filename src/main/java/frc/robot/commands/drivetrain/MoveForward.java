3cf5800ebad2b26ef72b2e1000a30a96347a7e7e
Thomas Baeyens <tbaeyens411@gmail.com>
2022-06-09 11:45:28 -0400

    Auton groups!

--- src/main/java/frc/robot/commands/drivetrain/MoveForward.java
+++ src/main/java/frc/robot/commands/drivetrain/MoveForward.java
@@ -39,7 +39,9 @@ public class MoveForward extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    DrivetrainSubsystem.stop();
+  }
 
   // Returns true when the command should end.
   @Override
